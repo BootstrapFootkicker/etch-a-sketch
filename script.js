ac9c0b46374aed8579a68d60a4962c4b365d6f84
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-10-08 23:09:46 -0600

    Add adjustable grid
    Deleted extra divs in html  that were causing unwanted grid size
    Added button to adjust size of grid
    Reworked Create grid to delete existing grid to create new one
    Changed entire grid class to id

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
-const mainContainer = document.getElementsByClassName("grid-container");
+const entireGridContainer = document.querySelector("#entire-grid-container");
+const gridContainer = document.getElementsByClassName("grid-container");
+const gridSizeButton = document.querySelector("#grid-sizing-button");
+const gridCollection = document.getElementsByClassName("gridBox");
 
-function addGridColor(gridBox){
-    gridBox.classList.add("hovered-grid-box")
+function addGridColor(gridBox) {
+    gridBox.classList.add("hovered-grid-box");
 }
 
-function  removeGridColor(gridBox){
-    gridBox.classList.remove("hovered-grid-box")
+function removeGridColor(gridBox) {
+    gridBox.classList.remove("hovered-grid-box");
 }
 
-for (let item of mainContainer) {
-    for (let i = 0; i < 16; i++) {
+function getButtonInput() {
+    let input = window.prompt("How many rows do you want");
+    createGrid(input);
+}
+
+
+function createGrid(rowNum) {
+    if (entireGridContainer.childNodes.length !== 0) {
+
+        console.log('not empty');
+        entireGridContainer.textContent = '';
+
+    }
+
+    for (let i = 0; i < rowNum; i++) {
         let newDiv = document.createElement("div");
         newDiv.id = 'r' + i;
-        newDiv.classList.add("gridBox");
-        item.appendChild(newDiv);
+        newDiv.classList.add("grid-container");
+        entireGridContainer.appendChild(newDiv);
+
     }
 
 
+    for (let item of gridContainer) {
+
+
+        for (let i = 0; i < rowNum; i++) {
+            let newDiv = document.createElement("div");
+            newDiv.id = 'r' + i;
+            newDiv.classList.add("gridBox");
+            item.appendChild(newDiv);
+
+        }
+
+    }
+    for (let item of gridCollection) {
+        item.addEventListener("mouseover", (event) => addGridColor(item));
+
+    }
 }
 
 
-const gridCollection = document.getElementsByClassName("gridBox");
+createGrid(16);
 
-for(let item of gridCollection){
-    item.addEventListener("mouseover", (event)=>addGridColor(item));
 
-}
-
+gridSizeButton.addEventListener("click", (event) => getButtonInput());
